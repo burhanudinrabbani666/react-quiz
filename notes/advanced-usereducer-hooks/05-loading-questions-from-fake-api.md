73d309f896d74a28c11e86bf29cb2c7fa4c45188
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-01-24 13:05:54 +0700

    feat: loading from fake api -  installing json server and fetching it

--- notes/advanced-usereducer-hooks/05-loading-questions-from-fake-api.md
+++ notes/advanced-usereducer-hooks/05-loading-questions-from-fake-api.md
@@ -1,1 +1,30 @@
 # LOADING FROM FAKE API
+
+Installing `json-server` for make fake api.
+
+```bash
+bun install json-server
+```
+
+```json
+{
+  // .................
+  "server": "json-server --watch data/questions.json --port 4000"
+  //............
+}
+```
+
+and after fetch to fake api for geting data
+
+```jsx
+const [state, dispatch] = useReducer(reducer, initialState);
+
+useEffect(() => {
+  fetch("http://localhost:4000/questions")
+    .then((res) => res.json())
+    .then((data) => dispatch({ type: "dataReceived", payload: data }))
+    .catch(() => dispatch({ type: "dataFailed" }));
+}, []);
+```
+
+[Next: Handling loading error and read](./06--handling-loading-error-and-read.md)
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
+import { useEffect, useReducer } from "react";
 import Header from "./components/Header";
 import Main from "./components/main";
 
+const initialState = {
+  question: [],
+  status: "loading", // "loading", "error", "ready", "active", "finished"
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        question: action.payload,
+        status: "ready",
+      };
+
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch(() => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div>
       <div className="app">
